e66367492990061724ec1387b4187a84721a23de
Amin Yaz <yazdi.mohammad99@gmail.com>
2021-12-11 14:30:51 +0330

    Add config file.

--- __init__.py
+++ __init__.py
@@ -2,11 +2,12 @@ from flask import Flask
 from flask_restful import Api
 from database.db import initialize_db
 from resources.routes import initialize_routes
+from config import config_map
 
 app = Flask(__name__)
 api = Api(app)
 app.config['MONGODB_SETTINGS'] = {
-    'host': 'mongodb://localhost/koole-poshti'
+    'host': config_map['mongodb_host']
 }
 
 initialize_db(app)
@@ -14,8 +15,8 @@ initialize_routes(api)
 
 @app.route('/')
 def index():
-    return "salam donya"
+    return "Hello World!"
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=config_map['debug_mode'], port=config_map['port'])
---
+++ config.py
@@ -0,0 +1,5 @@
+config_map = {
+    'debug_mode': True,
+    'port': 5000,
+    'mongodb_host': 'mongodb://localhost/koole-poshti'
+}
