adf22c5cefe64cb804a4d98c374ab5711719f640
Amin <yazdi.mohammad99@gmail.com>
2021-12-11 14:20:31 +0330

    Add Allow inheriance into models.

---
+++ database/models.py
@@ -0,0 +1,49 @@
+from .db import db
+
+class User(db.Document):
+    github_id = db.StringField(required=True)
+    beheshti_id = db.StringField()
+    first_name = db.StringField()
+    last_name = db.StringField()
+    avatar_url = db.StringField()
+    phone_number = db.IntField()
+
+
+class Tag(db.Document):
+    # hash
+    pass
+
+
+class File(db.Document):
+    url = db.StringField(required=True)
+
+
+class Post(db.Document):
+    _id = db.IntField(required=True, unique=True)
+    active = db.BooleanField(required=True)
+    created_at = db.DateTimeField(required=True)
+    likes = db.ReferenceField(User)
+    content = db.StringField()
+    tags = db.ListField(db.ReferenceField(Tag))
+    _file = db.ListField(db.ReferenceField(File))
+
+    meta = {'allow_inheritance': True}
+
+
+class GHPost(Post):
+    forks = db.IntField(required=True)
+    stars = db.IntField(required=True)
+    link = db.IntField(required=True)
+    meta_picture = db.StringField(required=True)
+    meta_title = db.StringField(required=True)
+    meta_description = db.StringField()
+
+
+
+""" class Bus(db.Document):
+    number_plate = db.StringField(required=True, unique=True)
+    manufacturer = db.StringField(required=True)
+    model = db.StringField(required=True)
+    year = db.IntField(required=True)
+    capcity = db.IntField(required=True)
+ """
