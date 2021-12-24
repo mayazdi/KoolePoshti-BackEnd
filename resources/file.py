82f25198326bb4a5fdd9da4b7120d5dfe5db98f6
Amin <yazdi.mohammad99@gmail.com>
2021-12-24 22:49:21 +0330

    Clean comments of file API.

--- resources/file.py
+++ resources/file.py
@@ -19,41 +19,10 @@ class FilesApi(Resource):
     def get(self, id):
         _file = File.objects.get(id=id)
         f = _file.data
-        # f = _file.data.read()
         content_type = _file.data.content_type
         filename = _file.data.filename
-        # return Response(f, mimetype=content_type, status=200, direct_passthrough=True)
         return send_file(
-                        io.BytesIO(f.read()),
-                        attachment_filename=filename,
-                        mimetype=content_type
-                )
-        """ with open("T.pdf", 'rb') as bites:
-            return send_file(
-                         io.BytesIO(bites.read()),
-                         attachment_filename="T.pdf",
-                         mimetype='application/pdf'
-                   ) """
-        """ return send_file(
-            path_or_file=f,
-            mimetype=content_type,
-            # as_attachment=True,
-            download_name=filename,
-            attachment_filename=filename
-        ) """
-        """ return send_file(
-                     io.BytesIO(f),
-                     attachment_filename='logo.jpeg',
-                     mimetype=content_type,
-                     as_attachment=True), 200 """
-        """ return send_file(
-            BytesIO(f),
-            attachment_filename=filename,
-            mimetype=content_type), 200 """
-        
-        
-        """ if post:
-            status_code = 200
-        else:
-            status_code = 404
-        return Response(post, mimetype='application/json', status=status_code) """
+                io.BytesIO(f.read()),
+                attachment_filename=filename,
+                mimetype=content_type
+            )
