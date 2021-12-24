c9a4eaa7139583120dbbb33c91e8ad051f13481f
Amin <yazdi.mohammad99@gmail.com>
2021-12-24 23:44:29 +0330

    Add index route & terms route + file.

---
+++ resources/main.py
@@ -0,0 +1,12 @@
+from flask import Response, request
+# from database.models import Post
+from flask_restful import Resource
+
+class MainApi(Resource):
+    def get(self):
+        return Response("<h1 style=\"text-align: center;\">Flask webserver working properly</h1>", status=200)
+
+class TermsApi(Resource):
+    def get(self):
+        with open("./terms.txt", "r") as f:
+            return Response(f.read(), status=200)
--- resources/routes.py
+++ resources/routes.py
@@ -1,6 +1,8 @@
 from .post import PostApi, PostsApi
-
+from .main import MainApi, TermsApi
 
 def initialize_routes(api, routing_prefix):
     api.add_resource(PostApi, routing_prefix + '/posts/<id>')
     api.add_resource(PostsApi, routing_prefix + '/posts')
+    api.add_resource(MainApi, routing_prefix + '/')
+    api.add_resource(TermsApi, routing_prefix + '/terms')
