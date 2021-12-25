08707395fd92265bd5dad526f28b12b16eeb2abb
Amin <yazdi.mohammad99@gmail.com>
2021-12-25 14:02:50 +0330

    Add CORS handlers.

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from flask_restful import Api
 from database.db import initialize_db
 from resources.routes import initialize_routes
 from config import config_map
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 api = Api(app)
 app.config['MONGODB_SETTINGS'] = {
     'host': config_map['mongodb_host']
--- resources/main.py
+++ resources/main.py
@@ -1,12 +1,15 @@
 from flask import Response, request
 # from database.models import Post
 from flask_restful import Resource
+from flask_cors import CORS, cross_origin
 
 class MainApi(Resource):
+    @cross_origin()
     def get(self):
         return Response("<h1 style=\"text-align: center;\">Flask webserver working properly</h1>", status=200)
 
 class TermsApi(Resource):
+    @cross_origin()
     def get(self):
         with open("./terms.txt", "r") as f:
             return Response(f.read(), status=200)
