baff6825d5c74f86210b7eb7543012d4e40b98c8
Amin <yazdi.mohammad99@gmail.com>
2021-12-25 16:25:39 +0330

    Add routes for comment.

--- resources/routes.py
+++ resources/routes.py
@@ -1,8 +1,11 @@
 from .post import PostApi, PostsApi
+from .comment import CommentApi, CommentsApi
 from .main import MainApi, TermsApi
 
 def initialize_routes(api, routing_prefix):
     api.add_resource(PostApi, routing_prefix + '/posts/<id>')
     api.add_resource(PostsApi, routing_prefix + '/posts')
+    api.add_resource(CommentsApi, routing_prefix + '/posts/<id>/comments')
+    api.add_resource(CommentApi, routing_prefix + '/posts/<id>/comments/<comment_id>')
     api.add_resource(MainApi, routing_prefix + '/')
     api.add_resource(TermsApi, routing_prefix + '/terms')
