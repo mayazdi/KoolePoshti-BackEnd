b0c996c4b2cefc04ec20bc7d34b68311375c8553
Amin <yazdi.mohammad99@gmail.com>
2022-01-01 00:31:25 +0330

    Add auth0 configs.

--- config.py
+++ config.py
@@ -2,5 +2,8 @@ config_map = {
     'routing_prefix': '/api/v1',
     'debug_mode': True,
     'server_port': 5000,
-    'mongodb_host': 'mongodb://localhost/koole-poshti'
+    'mongodb_host': 'mongodb://localhost/koole-poshti',
+    'auth0_domain': 'dev--rjfqqmo.us.auth0.com',
+    'api_audience': 'http://localhost:5000/api/v1/terms',
+    'algorithms': ["RS256"]
 }
