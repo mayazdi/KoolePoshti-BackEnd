892f63b0f0d366da2cd93e8d66dce6677f2901a2
Amin <yazdi.mohammad99@gmail.com>
2022-01-01 17:32:15 +0330

    Add routes for signin & login.

--- resources/routes.py
+++ resources/routes.py
@@ -1,6 +1,6 @@
 from .post import PostApi, PostsApi
 from .comment import CommentApi, CommentsApi
-from .main import MainApi, TermsApi
+from .main import MainApi, TermsApi, LoginApi, SigninApi
 from .file import FileApi, FilesApi
 from .tag import TagApi, CategoryApi
 
@@ -11,6 +11,8 @@ def initialize_routes(api, routing_prefix):
     api.add_resource(CommentApi, routing_prefix + '/posts/<id>/comments/<comment_id>')
     api.add_resource(MainApi, routing_prefix + '/')
     api.add_resource(TermsApi, routing_prefix + '/terms')
+    api.add_resource(LoginApi, routing_prefix + '/login')
+    api.add_resource(SigninApi, routing_prefix + '/signin')
     api.add_resource(FileApi, routing_prefix + '/file')
     api.add_resource(FilesApi, routing_prefix + '/file/<id>')
     api.add_resource(TagApi, routing_prefix + '/tag')
