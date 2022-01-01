d0e4b8295983186662af62a3134c80b1ac360a00
Amin <yazdi.mohammad99@gmail.com>
2022-01-01 17:33:49 +0330

    Add test API for login & Signin.

--- resources/main.py
+++ resources/main.py
@@ -1,8 +1,12 @@
-from flask import Response, request
+from flask import Response, request, jsonify
 # from database.models import Post
 from flask_restful import Resource
 from flask_cors import cross_origin
 from utils.auth0 import requires_auth
+from database.models import User
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
+from mongoengine.queryset.visitor import Q
+import hashlib
 
 class MainApi(Resource):
     @cross_origin(headers=["Content-Type", "Authorization"])
@@ -16,3 +20,43 @@ class TermsApi(Resource):
     def get(self):
         with open("./terms.txt", "r") as f:
             return Response(f.read(), status=200)
+
+
+class LoginApi(Resource):
+    def post(self):
+        body = request.get_json()
+        try:
+            _beheshti_email = body['beheshti_email']
+            _password = hashlib.md5(body['password'].encode()).hexdigest()
+        except Exception:
+            return "wrong format", 400
+        print("pedasag")
+        # user = User.objects.get(beheshti_email=_beheshti_email)
+        user = User.objects(Q(beheshti_email=_beheshti_email) & Q(password=_password))
+        if user:
+            access_token = create_access_token(identity=_beheshti_email)
+            return jsonify(access_token=access_token)
+        else:
+            return {"Error": "User | Password wrong"}, 406
+
+
+class SigninApi(Resource):
+    def post(self):
+        body = request.get_json()
+        try:
+            body['password'] = hashlib.md5(body['password'].encode()).hexdigest()
+            # body['password'] = body['password']
+            # print(body['password'])
+            # print(type(body['password']))
+        except Exception:
+            return "wrong", 400
+        user = User(**body).save()
+        id = user.id
+        return {'id': str(id)}, 201
+    
+    # @app.route("/protected", methods=["GET"])
+    @jwt_required()
+    def get(self):
+        current_user = get_jwt_identity()
+        print(current_user)
+        return {'logged_in_as' : current_user}, 200
