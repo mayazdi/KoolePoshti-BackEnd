4cc312b59d3264f42fa5a81dabbba0673830ccfa
Amin <yazdi.mohammad99@gmail.com>
2022-01-15 19:48:39 +0330

    Check user identity on Post routes.

--- resources/post.py
+++ resources/post.py
@@ -2,13 +2,14 @@ from flask import Response, request
 from database.models import Post
 from flask_restful import Resource
 from mongoengine.queryset.visitor import Q
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from utils.user import get_user_id, has_access
 
 class PostApi(Resource):
     decorators = [jwt_required()]
 
     def get(self, id):
-        post = Post.objects(Q(id=id) | Q(active=False)).to_json()
+        post = Post.objects(Q(id=id) | Q(active=True)).to_json()
         # post = Post.objects.get(id=id).to_json()
         if post:
             status_code = 200
@@ -17,14 +18,33 @@ class PostApi(Resource):
         return Response(post, mimetype='application/json', status=status_code)
     
     def put(self, id):
-        body = request.get_json()
-        Post.objects.get(id=id).update(**body)
-        return '', 200
+        try:
+            post = Post.objects.with_id(id)
+            if post:
+                if has_access(get_jwt_identity(), id):
+                    body = request.get_json()
+                    Post.objects.get(id=id).update(**body)
+                    return {'msg': 'Post deleted'}, 200
+                else:
+                    return {'error': 'Access denied'}, 403
+            else:
+                return {'error': 'Post Not found'}, 404
+        except:
+            pass
 
     def delete(self, id):
-        Post.objects.get(id=id).delete()
-        return '', 200
-
+        try:
+            post = Post.objects.with_id(id)
+            if post:
+                if has_access(get_jwt_identity(), id):
+                    post.delete()
+                    return {'msg': 'Post deleted'}, 200
+                else:
+                    return {'error': 'Access denied'}, 403
+            else:
+                return {'error': 'Post Not found'}, 404
+        except:
+            pass
 
 class PostsApi(Resource):
     decorators = [jwt_required()]
@@ -43,8 +63,10 @@ class PostsApi(Resource):
             posts = Post.objects().to_json()
         return Response(posts, mimetype="application/json", status=200)
     
-    def post(self):
+    def post(self):        
+        user_id = get_user_id(get_jwt_identity())
         body = request.get_json()
+        body['user'] = user_id
         post = Post(**body).save()
         id = post.id
         return {'id': str(id)}, 201
