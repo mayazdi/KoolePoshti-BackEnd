bb0795ee290655faa40200ef8574b64ef134ee5d
Amin <yazdi.mohammad99@gmail.com>
2022-01-15 15:58:19 +0330

    Add utils methods signature for db.

--- database/db.py
+++ database/db.py
@@ -5,3 +5,15 @@ db = MongoEngine()
 
 def initialize_db(app):
     db.init_app(app)
+
+def clear_db():
+    # Clear model collections
+    pass
+
+def initialize_categories():
+    # Default categories will be initialized here
+    pass
+
+def initialize_tags():
+    # Default tags will be initialized here
+    pass
