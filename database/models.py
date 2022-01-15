de0859987b0bd6e9e6bcfae6d83502b7c0e897cf
Amin <yazdi.mohammad99@gmail.com>
2022-01-15 15:57:49 +0330

    Change model cases to camelCase + Add required tags where needed + Minor changes.

--- database/models.py
+++ database/models.py
@@ -11,22 +11,22 @@ class File(db.Document):
 
 
 class User(db.Document):
-    github_id = db.StringField()
-    beheshti_email = db.EmailField()
-    activation_token = db.StringField()
+    githubId = db.StringField()
+    beheshtiEmail = db.EmailField(required=True)
+    # activation_token = db.StringField()
     active = db.BooleanField(required=True, default=False)
-    password = db.StringField()
-    first_name = db.StringField()
-    last_name = db.StringField()
+    password = db.StringField(required=True)
+    firstName = db.StringField(required=True)
+    lastName = db.StringField(required=True)
     avatar = db.ReferenceField(Image)
 
 
 class Category(db.Document):
-    title = db.StringField()
+    title = db.StringField(required=True)
 
 
 class Tag(db.Document):
-    name = db.StringField()
+    name = db.StringField(required=True)
     color = db.StringField(max_length=6, min_length=6)
     category = db.ReferenceField(Category)
 
@@ -38,18 +38,18 @@ class Like(db.Document):
 class Post(db.Document):
     # _id = db.IntField(required=True, unique=True)
     active = db.BooleanField(required=True, default=True)
-    created_at = db.DateTimeField(required=True, default=datetime.datetime.utcnow)
-    likes = db.ReferenceField(User)
-    content = db.StringField(db_field="cNT")
-    user = db.ListField(db.ReferenceField(User))
-    _file = db.ListField(db.ReferenceField(File), verbose_name="file")
+    createdAt = db.DateTimeField(required=True, default=datetime.datetime.utcnow)
+    user = db.ReferenceField(User, required=True)
+    content = db.StringField(required=True)
+    likes = db.ListField(db.ReferenceField(User))
+    file = db.ListField(db.ReferenceField(File))
 
     meta = {'allow_inheritance': True}
 
 
 class Comment(db.Document):
     content = db.StringField(required=True)
-    user = db.ReferenceField(User)
+    user = db.ReferenceField(User, required=True)
     post = db.ReferenceField(Post, required=True)
 
 
@@ -57,6 +57,6 @@ class GHPost(Post):
     forks = db.IntField(required=True)
     stars = db.IntField(required=True)
     url = db.IntField(required=True)
-    meta_picture = db.StringField(required=True)
-    meta_title = db.StringField(required=True)
-    meta_description = db.StringField()
+    metaPicture = db.StringField(required=True)
+    metaTitle = db.StringField(required=True)
+    metaDescription = db.StringField()
