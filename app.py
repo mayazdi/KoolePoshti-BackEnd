94ccba31ae23d561e95bb952eae66395893cbc36
Amin <yazdi.mohammad99@gmail.com>
2022-01-22 16:32:38 +0330

    Refactor app.py.

--- app.py
+++ app.py
@@ -8,21 +8,6 @@ from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_requir
 from database.db import clean_db, repopulater_tags
 import argparse
 
-app = Flask(__name__)
-CORS(app, support_credentials=True)
-# CORS(app, resources={r"/*": {"origins": "*"}})
-
-api = Api(app)
-app.config['MONGODB_SETTINGS'] = {
-    'host': config_map['mongodb_host']
-}
-app.config["JWT_SECRET_KEY"] = config_map['jwt_secret_key']
-app.config['JWT_TOKEN_LOCATION'] = ['headers', 'query_string']
-
-jwt = JWTManager(app)
-
-initialize_db(app)
-initialize_routes(api, config_map['routing_prefix'])
 
 
 if __name__ == "__main__":
@@ -34,5 +19,20 @@ if __name__ == "__main__":
         func = FUNCTION_MAP[args.command]
         func()
     except Exception as e:
+        app = Flask(__name__)
+        CORS(app, support_credentials=True)
+        # CORS(app, resources={r"/*": {"origins": "*"}})
+
+        api = Api(app)
+        app.config['MONGODB_SETTINGS'] = {
+            'host': config_map['mongodb_host']
+        }
+        app.config["JWT_SECRET_KEY"] = config_map['jwt_secret_key']
+        app.config['JWT_TOKEN_LOCATION'] = ['headers', 'query_string']
+
+        jwt = JWTManager(app)
+
+        initialize_db(app)
+        initialize_routes(api, config_map['routing_prefix'])
         app.run(port=config_map['server_port'], debug=config_map['debug_mode'])
         #threaded=True
