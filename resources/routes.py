a62d24e83d68ecf133eae522ec64f8bb3cc1df94
Amin <yazdi.mohammad99@gmail.com>
2022-01-29 00:26:23 +0330

    Routes: Reorder + chage some routes address.

--- resources/routes.py
+++ resources/routes.py
@@ -5,12 +5,21 @@ from .tag import TagApi, CategoryApi
 from .like import LikeApi, UnLikeApi
 from .main import MainApi, TermsApi, OGApi
 from .auth import SigninApi, SignupApi, ForgotApi, ActivateApi, ResetPasswordApi
+from .user import UserApi
 
 def initialize_routes(api, routing_prefix):
+    api.add_resource(SigninApi, routing_prefix + '/auth/signin')
+    api.add_resource(SignupApi, routing_prefix + '/auth/signup')
+    api.add_resource(ForgotApi, routing_prefix + '/auth/forgotPassword')
+    api.add_resource(ActivateApi, routing_prefix + '/auth/activate')
+    api.add_resource(ResetPasswordApi, routing_prefix + '/auth/resetPassword')
+    api.add_resource(UserApi, routing_prefix + '/user')
     api.add_resource(PostApi, routing_prefix + '/posts/<id>')
     api.add_resource(PostsApi, routing_prefix + '/posts')
     api.add_resource(CommentsApi, routing_prefix + '/posts/<id>/comments')
     api.add_resource(CommentApi, routing_prefix + '/posts/<id>/comments/<comment_id>')
+    api.add_resource(LikeApi, routing_prefix + '/posts/<id>/like')
+    api.add_resource(UnLikeApi, routing_prefix + '/posts/<id>/unlike')
     api.add_resource(MainApi, routing_prefix + '/')
     api.add_resource(TermsApi, routing_prefix + '/terms')
     api.add_resource(AvatarApi, routing_prefix + '/avatars')
@@ -19,13 +28,6 @@ def initialize_routes(api, routing_prefix):
     api.add_resource(FilesApi, routing_prefix + '/files/<id>')
     api.add_resource(FileDownloadApi, routing_prefix + '/files/<id>/download')
     api.add_resource(TagApi, routing_prefix + '/tag')
-    api.add_resource(CategoryApi, routing_prefix + '/category')
+    api.add_resource(CategoryApi, routing_prefix + '/categories')
     api.add_resource(OGApi, routing_prefix + '/og/<user>/<repository>')
-    api.add_resource(LikeApi, routing_prefix + '/posts/<id>/like')
-    api.add_resource(UnLikeApi, routing_prefix + '/posts/<id>/unlike')
-    api.add_resource(SigninApi, routing_prefix + '/auth/signin')
-    api.add_resource(SignupApi, routing_prefix + '/auth/signup')
-    api.add_resource(ForgotApi, routing_prefix + '/auth/forgot')
-    api.add_resource(ActivateApi, routing_prefix + '/auth/activate')
-    api.add_resource(ResetPasswordApi, routing_prefix + '/auth/reset')
-
+    
